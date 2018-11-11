572168bf3a79c4096da60192b8d8ddab1f213a7a
Kurt Werle <kurt@CircleW.org>
2018-11-11 13:07:07 -0800

    Install atom-ide-ui using deps.

--- src/welcome_notification.js
+++ src/welcome_notification.js
@@ -1,4 +1,5 @@
 // show welcome message on first installation
+const atomPackageDeps = require('atom-package-deps')
 const path = require('path')
 const fs = require('fs')
 const packageJSON = require('../package.json')
@@ -53,6 +54,7 @@ const checkRequirementsThenWelcome = () => {
   try {
     fs.accessSync(REQUIREMENTS_CHECK_PASSED)
   } catch (err) {
+    atomPackageDeps.install('ide-ruby', /* promptUser */ true)
     if (requirements.every(req => atom.packages.isPackageLoaded(req))) {
       atom.notifications.addSuccess(
         generateWelcomeMsg(),
